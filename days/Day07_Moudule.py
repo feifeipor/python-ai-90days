54a676694813090d07c247bc4db8e267be28da27
feifeidanger <fang13427138768@gmail.com>
2026-07-16 15:42:56 +0800

    feat: add python module learning

---
+++ days/Day07_Moudule.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+now=datetime.now()
+print(now.year)
+print(now.month)
+print(now.day)
+from random import randint
+relust=randint(1,100)
+print(relust)
+from calculator import add
+Relust=add(relust,relust)
+print(Relust)
+
---
+++ days/Day07_datetime.py
@@ -0,0 +1,7 @@
+import datetime
+
+
+now = datetime.datetime.now()
+
+if __name__ == "__main__":
+    print(now)
